e8001b96b51f36645f5b25f1472f0152359d673b
hirsty777 <777hirsty@gmail.com>
2023-07-25 10:57:54 +0400

    task 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//task 1  recursive function
+//task 1  recursive func with callback =============================================
 //option 1 
 // let base = null;
 // function expo(num, power, callback){
@@ -12,7 +12,6 @@
 
 // console.log(expo(5, 3, (num, base) => num*base ));
 // console.log(expo(3, 5, (num, base) => num*base ));
-
 
 // option 2 
 function callBack(num, pow){
@@ -28,3 +27,40 @@ const b = expo(2, 5, callBack);
 
 console.log(a);
 console.log(b);
+
+
+//task 2 fetch data ==============================================================
+//get data and show it on DOM
+async function getData(){
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const data = await response.json();
+        data.forEach(el=>{
+            document.body.append(createCard(el.title, el.body, el.id))  
+        })
+    }catch (error) {
+        console.log(error)
+    }
+
+    
+};
+getData();
+
+//ceate div object for fetched data
+function createCard(title, body, id){
+    const divEl = document.createElement("div");
+    const h1El = document.createElement("h1");
+    const pEl1 = document.createElement("p");
+    const pEl2 = document.createElement("p");
+
+    h1El.textContent=title.charAt(0).toUpperCase()+title.slice(1);
+    pEl1.textContent=`ID: ${id}`;
+    pEl2.textContent=body;
+
+    divEl.append(h1El, pEl1, pEl2);
+
+    return divEl
+};
+
+//task 3 deep copy promise =======================================================
+
